ef2687025add0b32ced5cab52335ddb2a4f67fad
akshita anand <anandakshita12@gmail.com>
2021-09-14 12:05:52 +0530

    common test

---
+++ ques.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+void algorithm(int arr[],int length)
+{
+    for(int i=0;i<length-1;i++)
+    {
+        for(int j=i+1;j<length;j++)
+        {
+            if(arr[i]>arr[j])
+            {
+                int temp=arr[i];
+                arr[i]=arr[j];
+                arr[j]=temp;
+            }
+        }
+    }
+    for(int k=0;k<length;k++){cout<<arr[k]<<" ";}
+}
+
+int main()
+{
+    int arr[]={83,1,45,95,45,53,11,47,0,45,67,82};
+    int length_of_arr=sizeof(arr)/sizeof(arr[0]);
+    algorithm(arr,length_of_arr);
+
+    return 0;
+}
